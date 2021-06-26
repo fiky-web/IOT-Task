b6dcb53e9bc5882c4d57a19f2098167baa7e604e
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2021-06-27 02:21:05 +0700

    add potensio and led project

---
+++ IOT/Basic Projects/PotensioWithLed/README.md
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<img src ="_Memproses nilai potensiometer ke terang redupnya LED.png" alt="Sampir">
+
+
+</body>
+</html>
+
---
+++ IOT/Basic Projects/PotensioWithLed/_memproses_nilai_potensiometer_ke_terang_redupnya_led1.ino
@@ -0,0 +1,16 @@
+int sensorVal = 0;
+void setup()
+{
+pinMode(A0, INPUT);
+pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+	sensorVal = analogRead(A0);
+  	digitalWrite(9,1);
+  	delay(sensorVal);
+  	digitalWrite(9,0);
+  	delay(sensorVal);
+  
+}
